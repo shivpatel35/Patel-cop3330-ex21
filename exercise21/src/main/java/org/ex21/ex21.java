61fdb762b8cd5dc3b0bc474f993b92a058b23735
shivpatel35 <84815860+shivpatel35@users.noreply.github.com>
2021-06-06 17:15:34 -0500

    Add files via upload

---
+++ exercise21/src/main/java/org/ex21/ex21.java
@@ -0,0 +1,60 @@
+package org.ex21;
+
+import java.util.Scanner;
+
+public class ex21 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the number of the month: ");
+        int month = scanner.nextInt();
+
+        switch(month){
+            case 1:
+                System.out.println("The name of the month is January.");
+                break;
+            case 2:
+                System.out.println("The name of the month is February.");
+                break;
+            case 3:
+                System.out.println("The name of the month is March.");
+                break;
+            case 4:
+                System.out.println("The name of the month is April.");
+                break;
+            case 5:
+                System.out.println("The name of the month is May.");
+                break;
+            case 6:
+                System.out.println("The name of the month is June.");
+                break;
+            case 7:
+                System.out.println("The name of the month is July.");
+                break;
+            case 8:
+                System.out.println("The name of the month is August.");
+                break;
+            case 9:
+                System.out.println("The name of the month is September.");
+                break;
+            case 10:
+                System.out.println("The name of the month is October.");
+                break;
+            case 11:
+                System.out.println("The name of the month is November.");
+                break;
+            case 12:
+                System.out.println("The name of the month is December.");
+                break;
+
+
+
+
+
+
+
+        }
+
+    }
+
+}
